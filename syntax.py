f58314a6ba47e32d97ea6a639a9bc50badab00fe
Hugh <hughandreivirganio@gmail.com>
2026-09-19 18:43:55 +1200

    the beginning, learnign what xml.etree is

---
+++ syntax.py
@@ -0,0 +1,3 @@
+import xml.etree.ElementTree as et # You can use lowercase 'et' or uppercase 'ET'
+# In this context, et is just a short nickname (called an alias) 
+# given to the xml.etree.ElementTree module when importing it
